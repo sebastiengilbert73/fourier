d7945bf040aab5385cde672d8f736e685872d5a8
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2024-03-17 16:58:32 -0400

    .\test\expand_signal.py: Adds 'quarter_odd'

--- test/expand_signal.py
+++ test/expand_signal.py
@@ -28,22 +28,25 @@ def main():
 
     a_n_odd, b_n_odd = expander.coefficients(signal, expansion_type='odd')
     a_n_even, b_n_even = expander.coefficients(signal, expansion_type='even')
+    a_n_quarter_odd, b_n_quarter_odd = expander.coefficients(signal, expansion_type='quarter_odd')
     a_n_quarter_even, b_n_quarter_even = expander.coefficients(signal, expansion_type='quarter_even')
 
-    n_max = 30 #len(a_n_odd) - 1
+    n_max = 50#len(a_n_odd) - 1
     delta_x = L/signal_length
     xs = np.arange(0, L, delta_x)
 
     reconstruction_odd = expander.reconstruct(a_n_odd[0: n_max + 1], b_n_odd[0: n_max + 1], 'odd', len(xs))
     reconstruction_even = expander.reconstruct(a_n_even[0: n_max + 1], b_n_even[0: n_max + 1], 'even', len(xs))
+    reconstruction_quarter_odd = expander.reconstruct(a_n_quarter_odd[0: n_max + 1], b_n_quarter_odd[0: n_max + 1], 'quarter_odd', len(xs))
     reconstruction_quarter_even = expander.reconstruct(a_n_quarter_even[0: n_max + 1], b_n_quarter_even[0: n_max + 1], 'quarter_even', len(xs))
 
     fig, ax = plt.subplots()
     ax.plot(xs, signal, label='Original signal', linewidth=2)
     ax.plot(xs, reconstruction_odd, label=f'Reconstruction, odd half-range (n_max = {n_max})', linewidth=2)
     ax.plot(xs, reconstruction_even, label=f'Reconstruction, even half-range (n_max = {n_max})', linewidth=2)
+    ax.plot(xs, reconstruction_quarter_odd, label=f'Reconstruction, odd quarter-range (n_max = {n_max})', linewidth=2)
     ax.plot(xs, reconstruction_quarter_even, label=f'Reconstruction, even quarter-range (n_max = {n_max})', linewidth=2)
-    ax.legend(loc='upper right')
+    ax.legend(loc='upper left')
     plt.show()
 
 
