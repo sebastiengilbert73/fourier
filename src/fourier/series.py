e87f1d8e4677ef53a47aab3b61c97fe437583030
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2024-03-17 14:11:48 -0400

    .\src\fourier\series.py .\test\expand_signal.py: 1st commit

---
+++ src/fourier/series.py
@@ -0,0 +1,84 @@
+import numpy as np
+import math
+from scipy import integrate
+
+def cos_vectorize(s):
+    return np.vectorize(math.cos)(s)
+
+def sin_vectorize(s):
+    return np.vectorize(math.sin)(s)
+
+class Expander:
+    def __init__(self, length):
+        self.L = length
+
+    def coefficients(self, signal, expansion_type='odd', maximum_n=1000):
+        if not type(signal) == np.ndarray:
+            raise ValueError(f"Expander.coefficients(): type(signal) = {type(signal)}. We expect np.ndarray.")
+        maximum_n = min(maximum_n, len(signal)//2)
+        if expansion_type == 'odd':
+            return self._half_range_odd(signal, maximum_n)
+        elif expansion_type == 'even':
+            return self._half_range_even(signal, maximum_n)
+        elif expansion_type == 'quarter_odd':
+            return self._quarter_range_odd(signal, maximum_n)
+        elif expansion_type == 'quarter_even':
+            return self._quarter_range_even(signal, maximum_n)
+        else:
+            raise NotImplementedError(f"Expander.coefficients(): Not implemented expansion type '{expansion_type}'")
+
+
+    def evaluate(self, a_n, b_n, x, expansion_type='odd'):
+        if expansion_type == 'odd':
+            s = 0
+            for n in range(len(b_n)):
+                s += b_n[n] * math.sin(n * math.pi * x/self.L)
+            return s
+        elif expansion_type == 'even':
+            s = a_n[0]
+            for n in range(1, len(a_n)):
+                s += a_n[n] * math.cos(n * math.pi * x/self.L)
+            return s
+        elif expansion_type == 'quarter_odd':
+            pass
+        elif expansion_type == 'quarter_even':
+            pass
+        else:
+            raise NotImplementedError(f"Expander.evaluate(): Not implemented expansion type '{expansion_type}'")
+
+    def reconstruct(self, a_n, b_n, expansion_type, signal_length):
+        delta_x = self.L/signal_length
+        xs = np.arange(0, self.L, delta_x)
+        reconstruction = np.zeros((signal_length), dtype=float)
+        for k in range(signal_length):
+            x = xs[k]
+            reconstruction[k] = self.evaluate(a_n, b_n, x, expansion_type)
+        return reconstruction
+
+    def _half_range_odd(self, signal, maximum_n):
+        a_n = np.zeros((maximum_n + 1), dtype=float)
+        b_n = np.zeros((maximum_n + 1), dtype=float)
+        delta_x = self.L/len(signal)
+        xs = np.arange(0, self.L + delta_x, delta_x)  # [0, dx, ..., L]
+        next_signal_value = signal[-1] + (signal[-1] - signal[-2])
+        extended_signal = np.concatenate((signal, np.array([next_signal_value])), axis=0)
+        for n in range(1, maximum_n + 1):
+            sinnpix_L = sin_vectorize(n * math.pi * xs/self.L)
+            b = 2.0 / self.L * integrate.simpson(y=(extended_signal * sinnpix_L), x=xs)
+            b_n[n] = b
+        return a_n, b_n
+
+    def _half_range_even(self, signal, maximum_n):
+        a_n = np.zeros((maximum_n + 1), dtype=float)
+        b_n = np.zeros((maximum_n + 1), dtype=float)
+        delta_x = self.L / len(signal)
+        xs = np.arange(0, self.L + delta_x, delta_x)  # [0, dx, ..., L]
+        # print(f"xs.shape = {xs.shape}; xs[-2] = {xs[-2]}; xs[-1] = {xs[-1]}")
+        next_signal_value = signal[-1] + (signal[-1] - signal[-2])
+        extended_signal = np.concatenate((signal, np.array([next_signal_value])), axis=0)
+        a_n[0] = 1.0/self.L * integrate.simpson(y=(extended_signal), x=xs)
+        for n in range(1, maximum_n + 1):
+            cosnpix_L = cos_vectorize(n * math.pi * xs/self.L)
+            a = 2.0/self.L * integrate.simpson(y=(extended_signal * cosnpix_L), x=xs)
+            a_n[n] = a
+        return a_n, b_n
---
+++ test/expand_signal.py
@@ -0,0 +1,49 @@
+import logging
+import sys
+sys.path.append("../src/fourier")
+import series as fourier_series
+import numpy as np
+import matplotlib.pyplot as plt
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s %(message)s')
+
+def main():
+    logging.info("expand_signal.main()")
+
+    L = 0.3
+    signal_length = 300
+    expander = fourier_series.Expander(L)
+
+
+    signal = np.zeros((signal_length), dtype=float)
+    for k in range(len(signal)):
+        if k < 50:
+            signal[k] = 5.0
+        if k > 100 and k < 150:
+            signal[k] = k/10.
+        if k > 200 and k < 280:
+            signal[k] = 25.0
+        if k > 280:
+            signal[k] = 17.0
+
+    a_n_odd, b_n_odd = expander.coefficients(signal, expansion_type='odd')
+    a_n_even, b_n_even = expander.coefficients(signal, expansion_type='even')
+
+    n_max = len(a_n_odd) - 1
+    delta_x = L/signal_length
+    xs = np.arange(0, L, delta_x)
+
+    reconstruction_odd = expander.reconstruct(a_n_odd, b_n_odd, 'odd', signal_length)
+    reconstruction_even = expander.reconstruct(a_n_even, b_n_even, 'even', signal_length)
+
+    fig, ax = plt.subplots()
+    ax.plot(xs, signal, label='Original signal', linewidth=2)
+    ax.plot(xs, reconstruction_odd, label=f'Reconstruction, odd half-range (n_max = {n_max})', linewidth=2)
+    ax.plot(xs, reconstruction_even, label=f'Reconstruction, even half-range (n_max = {n_max})', linewidth=2)
+    ax.legend(loc='upper right')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
