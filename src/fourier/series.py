e659e92cc5f85786a54d6a7d2b85577243e49d96
GMX\070038649 <segilber@ca.ibm.com>
2024-03-18 12:09:47 -0400

    .\src\fourier\series.py: Assumes the signal includes x=L

--- src/fourier/series.py
+++ src/fourier/series.py
@@ -53,8 +53,8 @@ class Expander:
             raise NotImplementedError(f"Expander.evaluate(): Not implemented expansion type '{expansion_type}'")
 
     def reconstruct(self, a_n, b_n, expansion_type, signal_length):
-        delta_x = self.L/signal_length
-        xs = np.arange(0, self.L, delta_x)
+        delta_x = self.L/(signal_length - 1)
+        xs = np.arange(0, self.L + delta_x/2, delta_x)
         reconstruction = np.zeros((signal_length), dtype=float)
         for k in range(signal_length):
             x = xs[k]
@@ -64,58 +64,49 @@ class Expander:
     def _half_range_odd(self, signal, maximum_n):
         a_n = np.zeros((maximum_n + 1), dtype=float)
         b_n = np.zeros((maximum_n + 1), dtype=float)
-        delta_x = self.L/len(signal)
-        xs = np.arange(0, self.L + delta_x, delta_x)  # [0, dx, ..., L]
-        next_signal_value = signal[-1] + (signal[-1] - signal[-2])
-        extended_signal = np.concatenate((signal, np.array([next_signal_value])), axis=0)
+        delta_x = self.L/(len(signal) - 1)
+        xs = np.arange(0, self.L + delta_x/2, delta_x)  # [0, dx, ..., L]
         for n in range(1, maximum_n + 1):
             sinnpix_L = sin_vectorize(n * math.pi * xs/self.L)
-            b = 2.0 / self.L * integrate.simpson(y=(extended_signal * sinnpix_L), x=xs)
+            b = 2.0 / self.L * integrate.simpson(y=(signal * sinnpix_L), x=xs)
             b_n[n] = b
         return a_n, b_n
 
     def _half_range_even(self, signal, maximum_n):
         a_n = np.zeros((maximum_n + 1), dtype=float)
         b_n = np.zeros((maximum_n + 1), dtype=float)
-        delta_x = self.L / len(signal)
-        xs = np.arange(0, self.L + delta_x, delta_x)  # [0, dx, ..., L]
-        next_signal_value = signal[-1] + (signal[-1] - signal[-2])
-        extended_signal = np.concatenate((signal, np.array([next_signal_value])), axis=0)
-        a_n[0] = 1.0/self.L * integrate.simpson(y=(extended_signal), x=xs)
+        delta_x = self.L / (len(signal) - 1)
+        xs = np.arange(0, self.L + delta_x/2, delta_x)  # [0, dx, ..., L]
+        a_n[0] = 1.0/self.L * integrate.simpson(y=(signal), x=xs)
         for n in range(1, maximum_n + 1):
             cosnpix_L = cos_vectorize(n * math.pi * xs/self.L)
-            a = 2.0/self.L * integrate.simpson(y=(extended_signal * cosnpix_L), x=xs)
+            a = 2.0/self.L * integrate.simpson(y=(signal * cosnpix_L), x=xs)
             a_n[n] = a
         return a_n, b_n
 
     def _quarter_range_odd(self, signal, maximum_n):
         a_n = np.zeros((maximum_n + 1), dtype=float)
         b_n = np.zeros((maximum_n + 1), dtype=float)
-        delta_x = self.L / len(signal)
-        xs = np.arange(0, self.L + delta_x, delta_x)  # [0, dx, ..., L]
-        next_signal_value = signal[-1] + (signal[-1] - signal[-2])
-        extended_signal = np.concatenate((signal, np.array([next_signal_value])), axis=0)
+        delta_x = self.L / (len(signal) - 1)
+        xs = np.arange(0, self.L + delta_x/2, delta_x)  # [0, dx, ..., L]
         for n in range(1, maximum_n + 1):
             sinnpix_2L = sin_vectorize(n * math.pi * xs/(2.0 * self.L))
-            b = 1.0/self.L * ((-1)**(n + 1) + 1) * integrate.simpson(y=(extended_signal * sinnpix_2L), x=xs)
+            b = 1.0/self.L * ((-1)**(n + 1) + 1) * integrate.simpson(y=(signal * sinnpix_2L), x=xs)
             b_n[n] = b
         return a_n, b_n
 
     def _quarter_range_even(self, signal, maximum_n):
         a_n = np.zeros((maximum_n + 1), dtype=float)
         b_n = np.zeros((maximum_n + 1), dtype=float)
-        delta_x = self.L / len(signal)
-        xs = np.arange(0, self.L + delta_x, delta_x)  # [0, dx, ..., L]
-        next_signal_value = signal[-1] + (signal[-1] - signal[-2])
-        extended_signal = np.concatenate((signal, np.array([next_signal_value])), axis=0)
-        f_L = next_signal_value
+        delta_x = self.L / (len(signal) - 1)
+        xs = np.arange(0, self.L + delta_x/2, delta_x)  # [0, dx, ..., L]
+        f_L = signal[-1]
         a_n[0] = f_L
         for n in range(1, maximum_n + 1):
             sinnpi_2 = sin_vectorize(n * math.pi/2)
             cosnpix_2L = cos_vectorize(n * math.pi * xs/(2 * self.L))
-            #a = -4.0 * f_L/(n * math.pi) * sinnpi_2 + 1.0/self.L * integrate.simpson(y=(extended_signal * cosnpix_2L), x=xs)
             t1 = -8.0 * self.L * f_L/(n * math.pi) * sinnpi_2
-            t2 = 2.0 * (1 - (-1)**n) * integrate.simpson(y=(extended_signal * cosnpix_2L), x=xs)
+            t2 = 2.0 * (1 - (-1)**n) * integrate.simpson(y=(signal * cosnpix_2L), x=xs)
             a = 1.0/(2 * self.L) * (t1 + t2)
             a_n[n] = a
         return a_n, b_n
