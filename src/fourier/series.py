bf9ae6dd21dda93cd673e7282ab75917b29187fa
GMX\070038649 <segilber@ca.ibm.com>
2024-03-20 09:07:38 -0400

    .\src\fourier\series.py: expansion_type is now a field of Expander

--- src/fourier/series.py
+++ src/fourier/series.py
@@ -9,122 +9,126 @@ def sin_vectorize(s):
     return np.vectorize(math.sin)(s)
 
 class Expander:
-    def __init__(self, length):
+    def __init__(self, length, expansion_type='odd'):
+        self.expansion_types = ['odd', 'even', 'quarter_odd', 'quarter_even']
+        if not expansion_type in self.expansion_types:
+            raise NotImplementedError(f"Expander.__init__(): Not implemented expansion type {expansion_type}. The valid expansion types are {self.expansion_types}.")
         self.L = length
+        self.expansion_type = expansion_type
 
-    def coefficients(self, signal, expansion_type='odd', maximum_n=1000):
+    def coefficients(self, signal, maximum_n=1000):
         if not type(signal) == np.ndarray:
             raise ValueError(f"Expander.coefficients(): type(signal) = {type(signal)}. We expect np.ndarray.")
         maximum_n = min(maximum_n, len(signal)//2)
-        if expansion_type == 'odd':
+        if self.expansion_type == 'odd':
             return self._half_range_odd(signal, maximum_n)
-        elif expansion_type == 'even':
+        elif self.expansion_type == 'even':
             return self._half_range_even(signal, maximum_n)
-        elif expansion_type == 'quarter_odd':
+        elif self.expansion_type == 'quarter_odd':
             return self._quarter_range_odd(signal, maximum_n)
-        elif expansion_type == 'quarter_even':
+        elif self.expansion_type == 'quarter_even':
             return self._quarter_range_even(signal, maximum_n)
         else:
-            raise NotImplementedError(f"Expander.coefficients(): Not implemented expansion type '{expansion_type}'")
+            raise NotImplementedError(f"Expander.coefficients(): Not implemented expansion type '{self.expansion_type}'")
 
 
-    def evaluate(self, a_n, b_n, x, expansion_type='odd'):
-        if expansion_type == 'odd':
+    def evaluate(self, a_n, b_n, x):
+        if self.expansion_type == 'odd':
             s = 0
             for n in range(1, len(b_n)):
                 s += b_n[n] * math.sin(n * math.pi * x/self.L)
             return s
-        elif expansion_type == 'even':
+        elif self.expansion_type == 'even':
             s = a_n[0]
             for n in range(1, len(a_n)):
                 s += a_n[n] * math.cos(n * math.pi * x/self.L)
             return s
-        elif expansion_type == 'quarter_odd':
+        elif self.expansion_type == 'quarter_odd':
             s = 0
             for n in range(1, len(b_n)):
                 s += b_n[n] * math.sin(n * math.pi * x/(2.0 * self.L))
             return s
-        elif expansion_type == 'quarter_even':
+        elif self.expansion_type == 'quarter_even':
             s = a_n[0]
             for n in range(1, len(a_n)):
                 s += a_n[n] * math.cos(n * math.pi * x/(2.0 * self.L))
             return s
         else:
-            raise NotImplementedError(f"Expander.evaluate(): Not implemented expansion type '{expansion_type}'")
+            raise NotImplementedError(f"Expander.evaluate(): Not implemented expansion type '{self.expansion_type}'")
 
-    def derivative(self, a_n, b_n, x, expansion_type='odd'):
-        if expansion_type == 'odd':
+    def derivative(self, a_n, b_n, x):
+        if self.expansion_type == 'odd':
             s = 0
             for n in range(1, len(b_n)):
                 s += b_n[n] * n * math.pi/self.L * math.cos(n * math.pi * x/self.L)
             return s
-        elif expansion_type == 'even':
+        elif self.expansion_type == 'even':
             s = 0
             for n in range(1, len(a_n)):
                 s += -a_n[n] * n * math.pi/self.L * math.sin(n * math.pi * x/self.L)
             return s
-        elif expansion_type == 'quarter_odd':
+        elif self.expansion_type == 'quarter_odd':
             s = 0
             for n in range(1, len(b_n)):
                 s += b_n[n] * n * math.pi/(2 * self.L) * math.cos(n * math.pi * x/(2.0 * self.L))
             return s
-        elif expansion_type == 'quarter_even':
+        elif self.expansion_type == 'quarter_even':
             s = 0
             for s in range(1, len(a_n)):
                 s += -a_n[n] * n * math.pi/(2 * self.L) * math.sin(n * math.pi * x/(2.0 * self.L))
             return s
         else:
-            raise NotImplementedError(f"Expander.derivative(): Not implemented expansion type '{expansion_type}'")
+            raise NotImplementedError(f"Expander.derivative(): Not implemented expansion type '{self.expansion_type}'")
 
-    def derivative_vector(self, a_n, b_n, number_of_points, expansion_type):
+    def derivative_vector(self, a_n, b_n, number_of_points):
         delta_x = self.L/(number_of_points - 1)
         xs = np.arange(0, self.L + delta_x/2, delta_x)
         deriv = np.zeros_like(xs)
         for k in range(len(xs)):
             x = xs[k]
-            deriv[k] = self.derivative(a_n, b_n, x, expansion_type)
+            deriv[k] = self.derivative(a_n, b_n, x)
         return deriv
 
-    def second_derivative(self, a_n, b_n, x, expansion_type='odd'):
-        if expansion_type == 'odd':
+    def second_derivative(self, a_n, b_n, x):
+        if self.expansion_type == 'odd':
             s = 0
             for n in range(1, len(b_n)):
                 s += -b_n[n] * (n * math.pi/self.L)**2 * math.sin(n * math.pi * x/self.L)
             return s
-        elif expansion_type == 'even':
+        elif self.expansion_type == 'even':
             s = 0
             for n in range(1, len(a_n)):
                 s += -a_n[n] * (n * math.pi/self.L)**2 * math.cos(n * math.pi * x/self.L)
             return s
-        elif expansion_type == 'quarter_odd':
+        elif self.expansion_type == 'quarter_odd':
             s = 0
             for n in range(1, len(b_n)):
                 s += -b_n[n] * (n * math.pi/(2 * self.L))**2 * math.sin(n * math.pi * x/(2.0 * self.L))
             return s
-        elif expansion_type == 'quarter_even':
+        elif self.expansion_type == 'quarter_even':
             s = 0
             for s in range(1, len(a_n)):
                 s += -a_n[n] * (n * math.pi/(2 * self.L))**2 * math.cos(n * math.pi * x/(2.0 * self.L))
             return s
         else:
-            raise NotImplementedError(f"Expander.second_derivative(): Not implemented expansion type '{expansion_type}'")
+            raise NotImplementedError(f"Expander.second_derivative(): Not implemented expansion type '{self.expansion_type}'")
 
-    def second_derivative_vector(self, a_n, b_n, number_of_points, expansion_type):
+    def second_derivative_vector(self, a_n, b_n, number_of_points, expansi):
         delta_x = self.L/(number_of_points - 1)
         xs = np.arange(0, self.L + delta_x/2, delta_x)
         second_deriv = np.zeros_like(xs)
         for k in range(len(xs)):
             x = xs[k]
-            second_deriv[k] = self.second_derivative(a_n, b_n, x, expansion_type)
+            second_deriv[k] = self.second_derivative(a_n, b_n, x)
         return second_deriv
 
-    def reconstruct(self, a_n, b_n, expansion_type, signal_length):
+    def reconstruct(self, a_n, b_n, signal_length):
         delta_x = self.L/(signal_length - 1)
         xs = np.arange(0, self.L + delta_x/2, delta_x)
         reconstruction = np.zeros((signal_length), dtype=float)
         for k in range(signal_length):
             x = xs[k]
-            reconstruction[k] = self.evaluate(a_n, b_n, x, expansion_type)
+            reconstruction[k] = self.evaluate(a_n, b_n, x)
         return reconstruction
 
     def _half_range_odd(self, signal, maximum_n):
