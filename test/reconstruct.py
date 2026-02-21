4d6915b430dd9f9526debe8ce059ecc9e600aa54
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2026-02-21 17:31:03 -0500

    .\test\reconstruct.py: Expands a simple signal

--- test/reconstruct.py
+++ test/reconstruct.py
@@ -10,7 +10,13 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s %(
 def main():
     logging.info("reconstruct.main()")
 
-    signal =
+    signal = np.array([0, 1, 0])
+    expander = fourier_series.Expander(length=1.0, expansion_type='odd')
+    a, b = expander.coefficients(signal, maximum_n=1000)
+    logging.info(f"a = {a}\n\tb = {b}")
+
+    reconstruction = expander.reconstruct(a, b, signal_length=len(signal))
+    logging.info(f"reconstruction = {reconstruction}")
 
 
 if __name__ == '__main__':
